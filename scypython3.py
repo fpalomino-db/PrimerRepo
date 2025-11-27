da5cf327f14ce3a18f16f42f4e0c9f55eeadbf17
Fabrizio Palomino <fabian_palomino20@hotmail.com>
2025-11-26 20:18:26 -0500

    Tercera version de la reposicion

---
+++ scypython3.py
@@ -0,0 +1,11 @@
+print("Hola, primer cambio al repositorio de git ")
+
+ListaEjemplo = [2,3,4,5,6,7]
+
+for elemento in ListaEjemplo:
+    print("El numero es: ")
+    print(elemento)
+print("Hola nuevo commit")
+
+
+print("Hola nuevo commit")
